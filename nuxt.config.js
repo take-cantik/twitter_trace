54b1ac72ffe2075c4ff2fbad24ccfd41c9247fff
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-05-19 04:42:17 +0900

    Add Plugins about firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    // use 'import firebase from "~/plugins/firebase.js"'
+    '~/plugins/firebase',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
