104084ea890cfbc39f4480ef94220151f1b8c503
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-05-21 04:37:32 +0900

    Add Header to default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <h1>Sider</h1>
-    <h1>Header</h1>
+    <Header />
     <Nuxt />
   </div>
 </template>
 
 <script>
+  import Header from '~/components/Header.vue'
+
   export default {
-    
+    components: {
+      Header,
+    }
   }
 </script>
 
