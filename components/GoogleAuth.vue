fdbe8df5b13dc826a0f8be598dad66317a29b33a
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-05-27 15:15:22 +0900

    Add GoogleAuth component

---
+++ components/GoogleAuth.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>v-if login</div>
+    <div>v-if logout</div>
+  </div>
+</template>
+
+<script>
+  import firebase from 'firebase'
+  import { db } from '@/firebase/firestore.js'
+</script>
--- components/Header.vue
+++ components/Header.vue
@@ -2,5 +2,17 @@
   <header>
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/mypage">MyPage</NuxtLink>
+    <google-auth />
   </header>
 </template>
+
+<script>
+  import GoogleAuth from '~/components/GoogleAuth.vue'
+
+  export default {
+    components: {
+      GoogleAuth,
+    }
+  }
+
+</script>
